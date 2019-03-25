4bb4ce36536ba582a8976096567745c4695386ea
suyalmukesh <suyalji85@whatever.com>
2019-03-25 16:48:28 +0530

    ver1

--- RegularExpressions/ReStartEnd.py
+++ RegularExpressions/ReStartEnd.py
@@ -11,7 +11,7 @@ import re
 s = input()
 v = input()
 for i,x in enumerate(s):
-    if re.match(v,s[i:]):  # searching from every ith next position 
+    if re.match(v,s[i:]):  # searching from every ith next position
         print((i,i+len(v)-1))
 if re.search(v, s)==None:
     print((-1,-1))
---
+++ RegularExpressions/re_substitution.py
@@ -0,0 +1,19 @@
+"""
+The re.sub() tool (sub stands for substitution) evaluates a pattern and,
+for each valid match,it calls a method (or lambda).
+The method is called for all matches and can be used to modify strings
+in different ways.The re.sub() method returns the modified string as an output.
+"""
+import re
+n = int(input())  # no of lines
+line = []
+for i in range(n):
+    line.append(input())
+
+for i in line:
+      i = (re.sub(r"\s[&]{2}\s",' and ',(re.sub(r"\s[|]{2}\s",' or ',i))))
+      print(re.sub(r"\s[&]{2}\s",' and ',(re.sub(r"\s[|]{2}\s",' or ',i))))
+
+  # I am doing the substitute operation twice for the consecutive replace
+  # like && && && or && && || ||
+
